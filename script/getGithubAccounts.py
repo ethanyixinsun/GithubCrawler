cd62ba2e1c4bd4c8e6d7262dcc525edeeb37c5d4
yixinsun <121929989@qq.com>
2023-03-09 22:00:53 -0800

    fix: seperate user_info and repo into 2 tables

--- script/getGithubAccounts.py
+++ script/getGithubAccounts.py
@@ -7,33 +7,30 @@ client = MongoClient(
     "mongodb+srv://yixinsun:19990802@githubcrawler.jxo2wfn.mongodb.net/test"
 )
 db = client.get_default_database()
-db.github_accounts.create_index("username", unique = True)
+db.user_info.create_index("username", unique=True)
+db.repository.create_index("name", unique=True)
 
 
 def store_user_data(username):
     user_url = f"https://api.github.com/users/{username}"
     user_data = requests.get(user_url).json()
     github_name = user_data["name"]
+    user_data_sliced = {"username": username, "name": github_name}
+    db.user_info.insert_one(user_data_sliced)
 
     github = Github()
     user = github.get_user(username)
-    repo_list = []
     for repo in user.get_repos():
-        repo_list.append(
-            {
-                "name": repo.full_name,
-                "description": repo.description,
-                "language": repo.language,
-                "stars": repo.stargazers_count,
-            }
-        )
-
-    user_data_sliced = {"username": username, "name": github_name, "repos": repo_list}
-    print(user_data_sliced)
-
-    db.github_accounts.insert_one(user_data_sliced)
+        repo_sliced = {
+            "name": repo.full_name,
+            "username": username,
+            "description": repo.description,
+            "language": repo.language,
+            "stars": repo.stargazers_count,
+        }
+        db.repository.insert_one(repo_sliced)
 
 
-file = open("./script/usernames.txt", 'r')
-for line in file: 
-    store_user_data(line.strip()) 
+file = open("./script/usernames.txt", "r")
+for line in file:
+    store_user_data(line.strip())
